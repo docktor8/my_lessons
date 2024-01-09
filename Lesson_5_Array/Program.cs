ff7bc6b7ab6b8d8830727fa238ad7832fc41c622
Aleksey_Dokin <docktor8@mail.ru>
2024-01-09 20:50:12 +0300

    Задание с моссивом

--- Lesson_5_Array/Program.cs
+++ Lesson_5_Array/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.ComponentModel;
+//Задание 1
 int[] arr = new int[5];
 arr[0] = 4;
 arr[1] = 5;
@@ -16,3 +17,17 @@ Console.WriteLine(arr[4]);
 int[] arr2 = new int[5] {1, 3, 7, 6, 8};
 
 int[] arr3 = {1, 100, 7, 9, 4};
+
+//ЗАдание 2
+int n =8;
+int[] arr1 = new int[n];
+int i = 0;
+
+while(i<arr1.Length)
+{
+    arr1[i] = i+1;
+    //Console.Write(arr[i]);
+    //Console.Write('');
+    Console.Write($"{arr1[i]} ");
+    i=i+1;
+}
