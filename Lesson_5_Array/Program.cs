7ebc0aeda0485f9c8a3c79ea97a8b8627af2ae7a
Aleksey_Dokin <docktor8@mail.ru>
2024-01-09 20:23:49 +0300

    Создал новую папку для уроков

---
+++ Lesson_5_Array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int[] arr = new int[5];
+arr[0] = 4;
+arr[1] = 5;
+arr[2] = 100;
+arr[3] = 1;
+arr[4] = 2;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+//Console.WriteLine(arr[5]);
+
+int[] arr2 = new int[5] {1, 3, 7, 6, 8};
+
+int[] arr3 = {1, 100, 7, 9, 4};
