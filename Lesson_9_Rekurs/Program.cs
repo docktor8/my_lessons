b3a51eec76cefaa685a22a89502e08e3182b7586
Aleksey_Dokin <docktor8@mail.ru>
2024-01-24 21:52:02 +0300

    Рекурсии

--- Lesson_9_Rekurs/Program.cs
+++ Lesson_9_Rekurs/Program.cs
@@ -4,7 +4,7 @@ int Fact(int n)
 {
     if(n==1 || n == 0)
     {
-        Console.WriteLine($"sTOP:{n}");
+        Console.WriteLine($"Stop:{n}");
         return 1;
     } 
     Console.WriteLine(n);
