4b4049d941346512717d4155d347a882091acabf
Aleksey_Dokin <docktor8@mail.ru>
2024-01-24 21:48:51 +0300

    Рекурсии

---
+++ Lesson_9_Rekurs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+int Fact(int n)
+{
+    if(n==1 || n == 0)
+    {
+        Console.WriteLine($"sTOP:{n}");
+        return 1;
+    } 
+    Console.WriteLine(n);
+    return n*Fact(n-1);
+}
+
+Console.WriteLine(Fact(5));
