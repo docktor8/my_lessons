de41b9fbe34b512c1d9a7ed1e1b95228fa2a1fe8
Aleksey_Dokin <docktor8@mail.ru>
2024-01-24 19:55:28 +0300

    Задания про строки

---
+++ Lesson_8_TooArray/Practic/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+// Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+/* Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. Сформировать строку, в которой слова расположены в обратном порядке. 
+В полученной строке слова должны быть также разделены пробелами. */
+
+char[,] arr_char = { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+string str = "И поэтому все так произошло";
+
+// Функция создающая строку из массива символов
+string GetLetters(char[,] arr_char)
+{
+    string letters = "";
+    foreach (char e in arr_char)
+    {
+        letters = letters + e;
+    }
+    return letters;
+}
+
+// Функция меняющая заглавные буквы на строчные
+string FormStrUppLow(string str)
+{
+    string str_low = str.ToLower();
+    return str_low;
+}
+
+// Функция прверяющая строку на палиндромность
+bool FindPalindr(string str)
+{
+    bool palind = false;
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] == str[str.Length - 1 - i])
+        {
+            palind = true;
+        }
+        else
+        {
+            palind = false;
+            break;
+        }
+    }
+    return palind;
+}
+
+// Функция меняющая расположение слов
+string FormStrRevers(string str)
+{
+    string str_rev = "";
+    string[] words = str.Split(new char[] { ' ' });
+    for (int i = (words.Length - 1); i >-1; i--)
+    {
+        str_rev += words[i] + " ";
+    }
+    return str_rev;
+}
+string result = FormStrRevers(str);
+Console.WriteLine(result);
--- Lesson_8_TooArray/Program.cs
+++ Lesson_8_TooArray/Program.cs
@@ -19,7 +19,7 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     Console.WriteLine();
 } */
 
-int[,] GretMatrix(int rowCount, int cntolumnCout)
+/* int[,] GretMatrix(int rowCount, int cntolumnCout)
 {
     int[,] matrix = new int[rowCount, cntolumnCout];
     for (int i = 0; i < matrix.GetLength(0); i++)
@@ -27,7 +27,7 @@ int[,] GretMatrix(int rowCount, int cntolumnCout)
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
 
-            matrix[i, j] = rnd.Next(1, 11);
+            matrix[i, j] = rnd.Next(1, 1000);
         }
     }
     return matrix;
@@ -46,5 +46,50 @@ void ShouMatrix(int[,] matrix)
     }
 }
 
-int[,] matrix = GretMatrix(4,5);
+int[,] matrix = GretMatrix(4, 5);
 ShouMatrix(matrix);
+//ShouMatrix(matrix);
+foreach (int e in matrix)
+{
+    if (IsInteresting(e))
+    {
+        Console.WriteLine(e);
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int sum = GetSum(value);
+    if (sum % 2 == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int GetSum(int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    return sum;
+}
+ */
+string GetLetters(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (Char.IsLetter(e))
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+string str = Console.ReadLine();
+string result = GetLetters(str);
+Console.WriteLine(result);
